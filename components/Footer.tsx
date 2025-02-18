1886979f805def8a6ece4c76154194c14e4fd61a
vikaskushwah738 <vikaskushwah738@gmail.com>
2025-02-18 14:30:10 +0530

    small updtaes

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,24 +1,32 @@
 import Link from "next/link"
-
+import Image from 'next/image';
+import Logo from '@/public/logo.png'
 const Footer = () => {
   return (
-    <footer className="md:flex md:justify-evenly ">
-          <div>
-            <h3>LOGO</h3>
-          </div>
-          <div>
-            <p>Usefull Link</p>
-            <ul className="space-y-2 pt-5">
-                    <li><Link href='/'>Home</Link></li>
-                    <li><Link href='/about'>About</Link></li>
-                    <li><Link href='/blog'>Blog</Link></li>
-                    <li><Link href='/contact-us'>Contact us</Link></li>
-                </ul>
-          </div>
-          <div>
-            <p>Contact us</p>
-          </div>
-          
+    <footer className="md:flex md:justify-evenly bg-[#151B1F] text-gray-50 py-4">
+      <div className="relative lg:h-24 lg:w-44 md:h-20 md:w-40 h-16 w-32">
+        <Image
+          alt="Eclat Engineering Consultants Logo"
+          src={Logo}
+          fill
+          style={{ objectFit: 'fill' }}
+          className="absolute  bg-white"
+          priority
+        />
+      </div>
+      <div>
+        <p>Usefull Link</p>
+        <ul className="space-y-2 pt-5">
+          <li><Link href='/'>Home</Link></li>
+          <li><Link href='/about'>About</Link></li>
+          <li><Link href='/blog'>Blog</Link></li>
+          <li><Link href='/contact-us'>Contact us</Link></li>
+        </ul>
+      </div>
+      <div>
+        <p>Contact us</p>
+      </div>
+
     </footer>
   )
 }
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,12 +1,22 @@
-import Link from "next/link"
+import Link from "next/link";
+import Image from 'next/image';
+import Logo from '@/public/logo.png';
 
 export const Navbar = () => {
     return (
         <header>
-            <nav className="flex justify-between px-16 h-16 items-center">
-                <div>
-                    Logo
+            <nav className="flex justify-between px-16 h-16 items-center bg-[#242931] text-gray-50">
+                <div className="relative lg:h-24 lg:w-44 md:h-20 md:w-40 h-16 w-32">
+                    <Image
+                        alt="Eclat Engineering Consultants Logo"
+                        src={Logo}
+                        fill
+                        style={{ objectFit: 'fill' }}
+                        className="absolute rounded-2xl bg-white"
+                        priority
+                    />
                 </div>
+
                 <ul className="flex space-x-10">
                     <li><Link href='/'>Home</Link></li>
                     <li><Link href='/about'>About</Link></li>
